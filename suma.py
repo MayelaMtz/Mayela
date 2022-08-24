90bfe2191177c11e41c81366f4051ac57cbacb07
MayelaMtz <111932715+MayelaMtz@users.noreply.github.com>
2022-08-24 06:18:54 -0500

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,17 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	i = int()
+	f = int()
+	suma = int()
+	suma = 0
+	print("Datos del rango")
+	print("Ingrese el valor inicial")
+	i = int(input())
+	while (i<=f):
+		suma = i+1
+	print("La suma del rango es : ",suma)
+
